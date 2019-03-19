b1ab6418c345d06683ab78c88e4e6462f1807b01
Rohit Singh <rohit.singh@rts.com.np>
2019-03-19 15:05:54 +0000

    sync3

--- app/src/main/java/com/rohsins/icetea/Lights.kt
+++ app/src/main/java/com/rohsins/icetea/Lights.kt
@@ -1,10 +1,13 @@
 package com.rohsins.icetea
 
+import android.graphics.Color
+import android.graphics.ColorMatrix
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.SeekBar
 import android.widget.TextView
+import android.widget.Toast
 
 class Lights : AppCompatActivity() {
 
@@ -15,5 +18,25 @@ class Lights : AppCompatActivity() {
         val linearLayout: LinearLayout = findViewById(R.id.lightsElement)
         val seekBar: SeekBar = findViewById(R.id.lightsSeekBar)
         val textView: TextView = findViewById(R.id.lightsTextView)
+
+        linearLayout.background.setTint(Color.parseColor("#54C1E2"))
+        textView.setText("Street Light")
+        seekBar.background.setTint(Color.parseColor("#ff0000"))
+
+        seekBar.setOnSeekBarChangeListener(object: SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                Toast.makeText(this@Lights, progress.toString(), Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                Toast.makeText(this@Lights, Color.parseColor("#000000").toString(), Toast.LENGTH_SHORT).show()
+//                seekBar?.setBackgroundColor(Color.parseColor("#00ff00"))
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+//                seekBar?.setBackgroundColor(Color.parseColor("#0000ff"))
+            }
+
+        })
     }
 }
