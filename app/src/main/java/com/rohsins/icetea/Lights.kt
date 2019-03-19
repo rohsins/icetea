f870379c9a849fbb9d95dad7dbf254c8398b832c
Rohit Singh <rohsins@gmail.com>
2019-03-19 08:32:17 +0545

    sync

--- app/src/main/java/com/rohsins/icetea/Lights.kt
+++ app/src/main/java/com/rohsins/icetea/Lights.kt
@@ -2,11 +2,18 @@ package com.rohsins.icetea
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.SeekBar
+import android.widget.TextView
 
 class Lights : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_lights)
+
+        val linearLayout: LinearLayout = findViewById(R.id.lightsElement)
+        val seekBar: SeekBar = findViewById(R.id.lightsSeekBar)
+        val textView: TextView = findViewById(R.id.lightsTextView)
     }
 }
