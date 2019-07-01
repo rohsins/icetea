55304caf2c3b149c7a45752c4ee1429036da8166
Rohit Singh <rohsins@gmail.com>
2019-07-01 22:02:22 +0545

    change credential

--- app/src/main/java/com/rohsins/icetea/Connectivity.kt
+++ app/src/main/java/com/rohsins/icetea/Connectivity.kt
@@ -19,9 +19,9 @@ import java.net.Socket
 import org.greenrobot.eventbus.EventBus
 
 private const val mqttURI = "tcp://hardware.wscada.net:1883" // fixed
-private const val mqttUserName = "rtshardware" // fixed
-private const val mqttPassword = "rtshardware" // fixed
-const val UDI = "918429f680052385" // Arbitrary
+private const val mqttUserName = "rohsins" // fixed
+private const val mqttPassword = "escapecharacters" // fixed
+const val UDI = "710829d688052940" // Arbitrary
 const val THINGCODE = 12001 //androidMobile
 private const val subscribeTopic = "RTSR&D/baanvak/sub/$UDI" // fixed
 private const val publishTopic = "RTSR&D/baanvak/pub/$UDI" // fixed
